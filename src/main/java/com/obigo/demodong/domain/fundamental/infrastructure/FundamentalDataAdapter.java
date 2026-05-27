b1847ed77d3abe7da11f425b4c434d9cfc346256
정동하 <156492383+jungdongha@users.noreply.github.com>
2026-05-27 11:40:12 +0900

    chore:import추가

--- src/main/java/com/obigo/demodong/domain/fundamental/infrastructure/FundamentalDataAdapter.java
+++ src/main/java/com/obigo/demodong/domain/fundamental/infrastructure/FundamentalDataAdapter.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 /**
  * {@link FundamentalDataPort} 구현체.
  *
